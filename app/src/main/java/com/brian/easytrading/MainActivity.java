84bff120873d5ae98ce01908ee256c773f3621d4
Brian Hsu <Brian_Hsu@asus.com>
2017-06-14 14:22:06 +0800

    first version

---
+++ app/src/main/java/com/brian/easytrading/MainActivity.java
@@ -0,0 +1,68 @@
+package com.brian.easytrading;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.demo_title_basic2);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ViewGroup tab = (ViewGroup) findViewById(R.id.tab);
+        View view = LayoutInflater.from(this).inflate(R.layout.demo_basic_title_offset_auto_center, tab, false);
+        tab.addView(view);
+//        tab.addView(LayoutInflater.from(this).inflate(R.layout.demo_basic_title_offset_auto_center, tab, false));
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+//        demo.setup(viewPagerTab);
+
+        FragmentPagerItems pages = new FragmentPagerItems(this);
+        for (int titleResId : tabs()) {
+            pages.add(FragmentPagerItem.of(getString(titleResId), DemoFragment.class));
+        }
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), pages);
+
+        viewPager.setAdapter(adapter);
+        viewPagerTab.setViewPager(viewPager);
+
+    }
+
+    public int[] tabs() {
+        return tab10();
+    }
+
+    public static int[] tab10() {
+        return new int[] {
+                R.string.demo_tab_1,
+                R.string.demo_tab_2,
+                R.string.demo_tab_3,
+                R.string.demo_tab_4,
+                R.string.demo_tab_5,
+                R.string.demo_tab_6,
+                R.string.demo_tab_7,
+                R.string.demo_tab_8,
+                R.string.demo_tab_9,
+                R.string.demo_tab_10
+        };
+    }
+}
